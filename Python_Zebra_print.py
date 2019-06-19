c40f74e8b493136b4a2fb4732cff333c43481b9e
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2019-06-19 00:56:32 -0700

    Created file

---
+++ Python_Zebra_print.py
@@ -0,0 +1,31 @@
+import win32print
+from subprocess import Popen, PIPE
+
+def get_online_printer():
+    # Uses Powershell to grab all online printers from 
+    process = Popen(['Powershell', '. "./online_printers";', '&getOnlinePrinters($_)'],
+        stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    stdout = stdout.strip()
+    print(stdout)
+    return stdout #stdout returns as bytecode. Decode it to utf8
+
+def send_document(filename):
+    try:
+        printer_choice = get_online_printer().decode("utf-8")
+        p = win32print.OpenPrinter(printer_choice)
+        print()
+        try:
+            #open printer, print, close out printer 
+            win32print.StartDocPrinter(p, 1,("Printer Test", None, "RAW"))
+            win32print.StartPagePrinter(p)
+            win32print.WritePrinter(p, filename)
+            win32print.EndPagePrinter(p)
+        except Exception as e:
+            print("Print Error: ", e)
+        win32print.ClosePrinter(p)
+    except Exception as e:
+        print("Could not find printer: ", e)
+        
+with open("path/to/file/with/zpl/commands.zpl.txt") as filename:
+    send_document(bytes(filename.read(), "utf-8"))
